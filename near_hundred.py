f3accc44a44491e26b6b41ca7722b7fa3abcc113
Steven Garis <stevengaris@gmail.com>
2019-02-10 11:18:48 -0600

    near_hundred

---
+++ near_hundred.py
@@ -0,0 +1,9 @@
+
+def near_hundred(num):
+    return (num >= 90 and num <= 110) or (num >= 190 and num <= 210)
+
+print(near_hundred(47))
+print(near_hundred(96))
+print(near_hundred(164))
+print(near_hundred(207))
+print(near_hundred(224))
