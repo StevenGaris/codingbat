7864ac403003e718d139a5a86a176e6c345ba9ca
Steven Garis <stevengaris@gmail.com>
2019-02-10 12:24:28 -0600

    string_times

---
+++ Warmup2/string_times.py
@@ -0,0 +1,9 @@
+# Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+def string_times(word, n):
+    return word * n
+
+print(string_times("Apple", 3))
+print(string_times("Fun", 10))
+print(string_times("Saxophone", 4))
+
