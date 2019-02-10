fbba337fc5f2bb9d5f24dd99cdac593a824166c3
Steven Garis <stevengaris@gmail.com>
2019-02-10 10:54:13 -0600

    monkey_trouble

---
+++ monkey_trouble.py
@@ -0,0 +1,8 @@
+
+def monkey_trouble(monkey_a, monkey_b):
+    return monkey_a == monkey_b
+
+print(monkey_trouble(True, True))
+print(monkey_trouble(True, False))
+print(monkey_trouble(False, True))
+print(monkey_trouble(False, False))
