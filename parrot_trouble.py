a1d2daf7c4ff46a4e4e45eb999a9c7506e76139d
Steven Garis <stevengaris@gmail.com>
2019-02-10 11:10:16 -0600

    parrot_trouble

--- parrot_trouble.py
+++ parrot_trouble.py
@@ -5,7 +5,7 @@ def parrot_trouble(talking, hour):
 print(parrot_trouble(True, 6))
 print(parrot_trouble(False, 6))
 print(parrot_trouble(True, 8))
-print(parrot_trouble(False, 12))
+print(parrot_trouble(False, 11))
 print(parrot_trouble(True, 19))
 print(parrot_trouble(True, 22))
 print(parrot_trouble(False, 22))
